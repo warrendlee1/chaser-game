601ae8a249d425df9c2d4725489fb7401bf7ce58
Warren Lee <warrendlee1@gmail.com>
2019-12-11 19:09:32 -0800

    Add files via upload

--- CMSI185_Assignment_WarrenLee.js
+++ CMSI185_Assignment_WarrenLee.js
@@ -1,13 +1,15 @@
 const progressBar = document.querySelector("progress");
 const prompt = document.querySelector("#prompt");
 let canvas;
-let gamestate = 0;
+let gamestate = "homepage";
 let backgroundImage;
-
-prompt.addEventListener('click', () => {
-  canvas.style.display = 'block';
-  prompt.style.display = 'none'; 
-  gamestate = 1;
+let victoryAudio = document.getElementById("victoryAudio");
+let lossAudio = document.getElementById("lossAudio");
+prompt.addEventListener("click", () => {
+  canvas.style.display = "block";
+  prompt.style.display = "none";
+  gamestate = "game";
+  console.log(`HAPPENED, gamestate is now ${gamestate}`);
 });
 
 class Sprite {
@@ -19,35 +21,42 @@ class Sprite {
   }
 }
 class Player extends Sprite {
-  constructor() {
-    super(width/10, height/2, "white", 20);
+  constructor(x, y, health, color) {
+    super(x, y, "white", 20);
+    this.health = health;
+    this.color = color;
+  }
+
+  reset() {
+    [this.x, this.y] = [width / 10, height / 2];
     this.health = 100;
   }
+
   move() {
     let speed = 4;
     if (keyIsDown(LEFT_ARROW)) {
-      if(this.x < 10){
+      if (this.x < 10) {
         speed = 0;
       }
       this.x -= speed;
     }
-  
+
     if (keyIsDown(RIGHT_ARROW)) {
-      if(this.x > width-10){
+      if (this.x > width - 10) {
         speed = 0;
       }
       this.x += speed;
     }
-  
+
     if (keyIsDown(UP_ARROW)) {
-      if(this.y < 10){
+      if (this.y < 10) {
         speed = 0;
       }
       this.y -= speed;
     }
-  
+
     if (keyIsDown(DOWN_ARROW)) {
-      if(this.y > height-10){
+      if (this.y > height - 10) {
         speed = 0;
       }
       this.y += speed;
@@ -55,33 +64,26 @@ class Player extends Sprite {
   }
   render() {
     fill(this.color);
-    circle(this.x,this.y, this.diameter);
+    circle(this.x, this.y, this.diameter);
   }
-  
   takeHit() {
     this.health -= 1;
     progressBar.value = this.health;
   }
 }
-// class PlayerTail extends Sprite {
-//   constructor(x, y, Xspeed, Yspeed) {
-//     super(x, y, randomColor(), 10)
-//     this.Xspeed = Xspeed;
-//     this.Yspeed = Yspeed;
-//   }
-//   render() {
-//     circle(this.x, this.y, this.diameter);
-//   }
-//   move(player){
-//     this.x += (player.x - this.x) *  this.Xspeed;
-//     this.y += (player.y - this.y) *  this.Yspeed;
-//   }
-// }
 class Enemy extends Sprite {
   constructor(x, y, Xspeed, Yspeed) {
     super(x, y, randomColor(), 30);
     this.Xspeed = Xspeed;
-    this.Yspeed = Yspeed
+    this.Yspeed = Yspeed;
+  }
+  reset() {
+    [this.x, this.y, this.Xspeed, this.Yspeed] = [
+      width * 5 / 7,
+      height / 2,
+      this.Xspeed,
+      this.Yspeed
+    ];
   }
   render() {
     fill(this.color);
@@ -90,30 +92,33 @@ class Enemy extends Sprite {
   move() {
     this.x += this.Xspeed;
     this.y += this.Yspeed;
-    if(this.x >= width-this.diameter/2 || this.x <= this.diameter/2){
+    if (this.x >= width - this.diameter / 2 || this.x <= this.diameter / 2) {
       this.Xspeed = -this.Xspeed;
     }
-    if(this.y >= height-this.diameter/2 || this.y <= this.diameter/2){
+    if (this.y >= height - this.diameter / 2 || this.y <= this.diameter / 2) {
       this.Yspeed = -this.Yspeed;
     }
   }
 }
 class Enemy2 extends Sprite {
   constructor(x, y, Xspeed, Yspeed) {
-    super(x, y, randomColor(), 60)
+    super(x, y, randomColor(), 60);
     this.Xspeed = Xspeed;
     this.Yspeed = Yspeed;
+  }
+  reset() {
+    [this.x, this.y] = [width * 6 / 8, height / 2];
   }
   render() {
     circle(this.x, this.y, this.diameter);
   }
   move(target) {
-    this.x += (target.x - this.x) *  this.Xspeed;
-    this.y += (target.y - this.y) *  this.Yspeed;
-    if(this.x >= width-this.diameter/2 || this.x <= this.diameter/2){
+    this.x += (target.x - this.x) * this.Xspeed;
+    this.y += (target.y - this.y) * this.Yspeed;
+    if (this.x >= width - this.diameter / 2 || this.x <= this.diameter / 2) {
       this.Xspeed = -this.Xspeed;
     }
-    if(this.y >= height-this.diameter/2 || this.y <= this.diameter/2){
+    if (this.y >= height - this.diameter / 2 || this.y <= this.diameter / 2) {
       this.Yspeed = -this.Yspeed;
     }
   }
@@ -123,7 +128,7 @@ class Goal {
     Object.assign(this, { x, y, width, height, color });
   }
   isTouched(player) {
-    return player.x >= width-30;
+    return player.x >= width - 30;
   }
   render() {
     let goal_width = 30;
@@ -136,27 +141,28 @@ let width = 1500;
 let height = 750;
 let goal = new Goal();
 let points = 0;
-
-
 let scarecrow;
-
-
-// let playerTail = new PlayerTail(...randomParticle(), 0.01);
-let player = new Player();
-let enemies = []; 
+let numScarecrow = 3;
+let player = new Player(width / 10, height / 2, 100, "white");
+let enemies = [];
 let enemies2 = [];
 
-for (let i = 0; i<200; i++){
-  enemies.push(new Enemy(...randomParticle()))
+for (let i = 0; i < 200; i++) {
+  enemies.push(new Enemy(...randomParticle()));
+  console.log(enemies[i].Xspeed);
 }
-for (let i = 0; i<5; i++){
-  enemies2.push(new Enemy2(...randomPointOnCanvas(), Math.random()*0.01, Math.random()*0.01))
+for (let i = 0; i < 5; i++) {
+  enemies2.push(
+    new Enemy2(
+      ...randomPointOnCanvas(),
+      Math.random() * 0.01,
+      Math.random() * 0.01
+    )
+  );
 }
 
 function preload() {
-  backgroundImage = loadImage(
-   "https://i.imgur.com/TmeMFKz.jpg"
-  );
+  backgroundImage = loadImage("https://i.imgur.com/TmeMFKz.jpg");
 }
 
 function moveToward(leader, follower, speed) {
@@ -175,18 +181,15 @@ function collided(sprite1, sprite2) {
 
 function randomParticle() {
   return [
-    width*(5/7),
-    height/2,
-    Math.floor(Math.random()*2) - 0.5,
-    Math.floor(Math.random()*2) - 0.5
+    width * (5 / 7),
+    height / 2,
+    Math.floor(Math.random() * 2) - 0.5,
+    Math.floor(Math.random() * 2) - 0.5
   ];
 }
 
 function randomPointOnCanvas() {
-  return [
-    width*(5/7),
-    height/2
-  ];
+  return [width * (5 / 7), height / 2];
 }
 
 function checkForDamage(enemy, player) {
@@ -198,7 +201,7 @@ function checkForDamage(enemy, player) {
 function adjust() {
   const characters = [player, ...enemies, ...enemies2];
   for (let i = 0; i < characters.length; i++) {
-    for (let j = i+1; j < characters.length; j++) {
+    for (let j = i + 1; j < characters.length; j++) {
       pushOff(characters[i], characters[j]);
     }
   }
@@ -234,58 +237,113 @@ function setup() {
 function slowDown(sprite) {
   sprite.Xspeed = sprite.Xspeed;
   sprite.Yspeed = sprite.Yspeed;
-  if(keyIsDown(32)){
+  if (keyIsDown(32)) {
     sprite.Xspeed = sprite.Xspeed * 0.95;
     sprite.Yspeed = sprite.Yspeed * 0.95;
   }
-  
 }
 
-function renderScarecrow(){
-  fill('rgba(255, 255, 255, 0.6');
-  circle(this.x,this.y, this.diameter);
-}
 function draw() {
-  background('black');
+  background("black");
   noStroke();
-  if (gamestate === 1) {
+  if (gamestate === "game") {
+    pauseAudio(victoryAudio);
     goal.render();
     player.render();
     player.move();
+
     enemies.forEach(enemy1 => {
       enemy1.render();
+    });
+    enemies.forEach(enemy1 => {
       checkForDamage(enemy1, player);
+    });
+    enemies.forEach(enemy1 => {
       enemy1.move();
+    });
+    enemies.forEach(enemy1 => {
       slowDown(enemy1);
     });
-    enemies2.forEach(enemy2 => {enemy2.render();})
-    enemies2.forEach(enemy2 => {checkForDamage(enemy2,player);})
-    enemies2.forEach(enemy2 => {slowDown(enemy2);})
-    enemies2.forEach(enemy2 => {enemy2.move(scarecrow || player);})
-    
-    if(scarecrow){
-      console.log("hi")
+
+    enemies2.forEach(enemy2 => {
+      enemy2.render();
+    });
+    enemies2.forEach(enemy2 => {
+      checkForDamage(enemy2, player);
+    });
+    enemies2.forEach(enemy2 => {
+      slowDown(enemy2);
+    });
+    enemies2.forEach(enemy2 => {
+      enemy2.move(scarecrow || player);
+    });
+
+    if (scarecrow) {
       scarecrow.render();
-      scarecrow.ttl--; 
+      scarecrow.ttl--;
       if (scarecrow.ttl < 0) {
         scarecrow = undefined;
       }
     }
-    if(goal.isTouched(player)){
-      gamestate = 2;
+    if (player.health <= 0) {
+      gamestate = "loss";
+    }
+    if (goal.isTouched(player)) {
+      gamestate = "win";
     }
     adjust();
   }
 
-  if(gamestate === 2) {
-    console.log("you win");
-    
+  if (gamestate === "win") {
+    playAudio(victoryAudio);
+    canvas.style.display = "none";
+    prompt.textContent = "YOU WIN PLAY AGAIN";
+    prompt.style.display = "block";
+    player.reset();
+    enemies.forEach(enemy1 => {
+      enemy1.reset();
+    });
+    enemies2.forEach(enemy2 => {
+      enemy2.reset();
+    });
+    numScarecrow = 2;
+    gamestate = "homepage";
+  }
+
+  if (gamestate === "loss") {
+    playAudio(lossAudio);
+    canvas.style.display = "none";
+    prompt.textContent = "TRY AGAIN";
+    prompt.style.display = "block";
+    player.reset();
+    enemies.forEach(enemy1 => {
+      enemy1.reset();
+    });
+    enemies2.forEach(enemy2 => {
+      enemy2.reset();
+    });
+    numScarecrow = 2;
+    gamestate = "homepage";
   }
 }
 
+if (gamestate === "homepage") {
+  console.log("winaudio");
+  pauseAudio(victoryAudio);
+}
+
+function playAudio(x) {
+  x.play();
+}
+
+function pauseAudio(x) {
+  x.pause();
+}
+
 function mouseClicked() {
-  if (!scarecrow) {
-    scarecrow = new Player(Math.random()*width, Math.random()*height, "white", 10, 0);
+  if (!scarecrow && numScarecrow > 0) {
+    numScarecrow -= 1;
+    scarecrow = new Player(player.x, player.y, 100, "rgba(255,255,255,0.3)");
     scarecrow.ttl = frameRate() * 5;
   }
 }
